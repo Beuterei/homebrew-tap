b9ad5f71877c93683fb55e1848e25163c7e370b4
Luis Beu <me@luisbeu.de>
2021-03-20 21:41:43 +0100

    update _final

--- Formula/_final.rb
+++ Formula/_final.rb
@@ -1,26 +1,17 @@
 class Final < Formula
   desc "Script for finding the final name"
   homepage "https://github.com/beuluis/_final-REMASTERED"
-  url "https://github.com/beuluis/_final-REMASTERED/archive/v0.0.1.tar.gz"
-  sha256 "22e6d6c96622631752c93ec4b89e482e9786eb1dd4294fbd00680aeb2eb94e13"
+  url "https://github.com/beuluis/_final-REMASTERED/archive/v0.0.2.tar.gz"
+  sha256 "bc7e13d9801e4286cffe79bad9e066184b3b2d6d34407c6045bb0669c3e26a45"
 
-  depends_on "bash" => :run
+  depends_on "bash"
 
   def install
-    inreplace "_final.sh" do |s|
-      s.sub! "modifiers.txt", "#{share}/modifiers.txt"
-      s.sub! "separators.txt", "#{share}/separators.txt"
-      s.sub! "config.defaults.conf", "#{share}/config.defaults.conf"
-    end
-    bin.install "_final.sh" => "_final"
-
-    share.install "modifiers.txt"
-    share.install "separators.txt"
-    share.install "config.defaults.conf"
+    bin.install "_final"
   end
 
   test do
-    run_output = shell_output("#{bin}/_final HelloWorld.js>&1")
+    run_output = shell_output("#{bin}/_final -e HelloWorld.js>&1")
     assert_match /^HelloWorld.*.js$/, run_output
   end
 end
