9f086d0a827be2f5c67ed5c7c8830b0755a76610
Luis Beu <me@luisbeu.de>
2022-02-21 03:13:24 +0100

    add new wnv release

--- Formula/wnv.rb
+++ Formula/wnv.rb
@@ -1,8 +1,8 @@
 class Wnv < Formula
   desc "Script for automatically switch node versions based on the configured engine the the package.json"
   homepage "https://github.com/beuluis/what-node-version"
-  url "https://github.com/beuluis/what-node-version/archive/v0.0.1.tar.gz"
-  sha256 "d25aca7703a3d0fa5088c85bdf01f3ac370244e04a39d479f435767e454fb62b"
+  url "https://github.com/beuluis/what-node-version/archive/v0.0.2.tar.gz"
+  sha256 "267c0d3353bceb61f13ac3022e564098042baf12d9d392e7e66b8747c11e6e44"
 
   depends_on "bash"
   depends_on "jq"
