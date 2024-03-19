b708ee5a81ed07db4e253a015c99f15bb49a8aa7
Luis Beu <me@luisbeu.de>
2024-03-19 02:32:05 +0100

    Update tag

--- Formula/hpm.rb
+++ Formula/hpm.rb
@@ -1,7 +1,7 @@
 class Hpm < Formula
   desc "A package manager build on top of hacs"
   homepage "https://github.com/Beuterei/hacs-package-manager"
-  url "https://github.com/Beuterei/hacs-package-manager/releases/download/0.0.1/hpm"
+  url "https://github.com/Beuterei/hacs-package-manager/releases/download/0.0.2/hpm"
   sha256 "e39f37c2ec559cc68448ea2044df41015f353a606ae1481e09b0502548d944f3"
 
   depends_on "bash"
